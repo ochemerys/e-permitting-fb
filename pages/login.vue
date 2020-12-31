689c549960acdb67c5c3b6153e46f6601c78e139
Oleks Chemerys <oleks@quick.cz>
2020-12-31 16:31:21 -0700

    login / logout implementation

--- pages/login.vue
+++ pages/login.vue
@@ -1,50 +1,87 @@
 <template>
-  <div>
-    <h1>Login</h1>
-    <form @submit.prevent>
-      <div>
-        <input v-model="account.email" type="email" placeholder="Enter email">
-      </div>
-      <div>
-        <input v-model="account.password" type="password" placeholder="Enter password">
-      </div>
-      <button @click="onSubmit">
+  <v-card width="400" class="mx-auto mt-5" data-test-id="login-card">
+    <v-card-title class="headline">
+      Login
+    </v-card-title>
+    <v-card-text>
+      <v-form ref="form" lazy-validation @submit.prevent>
+        <v-text-field
+          v-model="userEmail"
+          data-test-id="login-form-email"
+          type="email"
+          label="User Email"
+          prepend-icon="mdi-account-circle"
+          :rules="[rules.required, rules.email]"
+          required
+        />
+        <v-text-field
+          v-model="userPassword"
+          data-test-id="login-form-password"
+          :type="showPassword ? 'text': 'password'"
+          label="Password"
+          prepend-icon="mdi-lock"
+          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+          :rules="[rules.required]"
+          required
+          @click:append="showPassword=!showPassword"
+        />
+      </v-form>
+    </v-card-text>
+    <v-divider />
+    <v-card-actions>
+      <v-spacer />
+      <v-btn color="info" data-test-id="login-form-submit" @click="onSubmitForm">
         Submit
-      </button>
-    </form>
-    <div class="error">
-      {{ error.message }}
-    </div>
-  </div>
+      </v-btn>
+      <v-btn color="default" data-test-id="login-form-cancel" @click="onCancel">
+        Cancel
+      </v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
 export default {
   data () {
     return {
-      account: {
-        email: '',
-        password: ''
-      },
-      error: ''
+      showPassword: false,
+      userEmail: '',
+      userPassword: '',
+      rules: {
+        required: value => !!value || 'Required.',
+        email: (value) => {
+          const pattern = /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,5})$/
+          return pattern.test(value) || 'Invalid e-mail'
+        }
+      }
     }
   },
   methods: {
-    async onSubmit () {
+    onCancel () {
+      // e.preventDefault();
+      this.$router.push('/')
+    },
+    async onSubmitForm () {
+      const isValid = this.$refs.form.validate()
+      if (!isValid) { return }
+
       try {
-        // TODO: sanitize email and password values
-        await this.$store.dispatch('users/login', this.account)
-        this.$router.push('/admin')
+        const payload = {
+          email: this.userEmail,
+          password: this.userPassword
+        }
+        await this.$store.dispatch('users/login', payload)
+        this.$router.push('/permitting')
       } catch (err) {
-        this.error = err
+        // TODO: snackbar
+        // eslint-disable-next-line no-console
+        console.log('ERROR:', err)
       }
     }
   }
 }
 </script>
 
-<style lang="scss" scoped>
-  input {
-    color: white;
-  }
+<style scoped>
+
 </style>
---
+++ pages/logout.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  created () {
+    this.onLogout()
+  },
+  methods: {
+    async onLogout () {
+      try {
+        await this.$store.dispatch('users/logout')
+        this.$router.push('/')
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.log('ERROR:', error)
+      }
+    }
+  }
+}
+</script>
